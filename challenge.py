d094c718bee033c7d7cf5aec3c848b9a32c61d0d
Mike Moss <doomsday.wombats@gmail.com>
2020-01-17 21:04:21 -0800

    parsing numbers successfully? need more unit tests...

--- challenge.py
+++ challenge.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python3
+import re
 
-def evalulate_line(line):
+def parse_fraction(lexeme):
+	exprs=['[-]?[\d]+[_][-]?[\d]+[/][-]?[\d]+','[-]?[\d]+[/][-]?[\d]+','[-]?[\d]+']
+
+	for expr in exprs:
+		if re.findall(expr,lexeme):
+			parts=[int(num) for num in re.findall(exprs[-1],lexeme)]
+			while len(parts)<3:
+				parts=[0]+parts
+			return parts
+
+	return None,None,None
+
+def evaluate_line(line):
+	lexemes=line.strip().split()
+	for lexeme in lexemes:
+		whole,num,top=parse_fraction(lexeme)
+		print(whole,num,top)
+
 	return 'fail'
 
 def unit_tests():
@@ -10,12 +28,14 @@ def unit_tests():
 	for test,answer in tests:
 		print('Testing "%s" == "%s"'%(test,answer))
 
-		calculated=evalulate_line(test)
+		calculated=evaluate_line(test)
 
 		if calculated==answer:
 			print('\t PASS')
 		else:
 			print('\t FAIL (GOT "%s")'%calculated)
+
+		print('')
 
 if __name__=='__main__':
 	try:
